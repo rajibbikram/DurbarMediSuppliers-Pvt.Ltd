441fa9333fbe2bd3e4839accf10db843d3b47c5a
rajib <shahrajib278@gmail.com>
2026-09-02 08:36:38 +0545

    inital commit

--- durbar-frontend/src/components/Contact.jsx
+++ durbar-frontend/src/components/Contact.jsx
@@ -133,7 +133,7 @@ const Contact = () => {
               </div>
             </div>
           </div>
-          
+{/*           
           <div className="lg:col-span-3">
             <div className="bg-white rounded-2xl shadow-lg p-6 md:p-8 border border-gray-100">
               <h3 className="text-xl md:text-2xl font-bold mb-4 md:mb-6 text-gray-900">Send us a Message</h3>
@@ -245,7 +245,7 @@ const Contact = () => {
                 </button>
               </form>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
